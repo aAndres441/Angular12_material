1b923305cf79e538419477598df6df5715d0a433
Andres <andrarias@gmail.com>
2021-07-06 20:14:50 -0300

    untag

---
+++ borrar/src/app/craerFicheros/pages/agregar/agregar.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FicherosComponent } from './ficheros/ficheros.component';
+
+
+
+@NgModule({
+  declarations: [
+    FicherosComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AgregarModule { }
---
+++ borrar/src/app/craerFicheros/pages/agregar/ficheros/ficheros.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ficheros',
+  templateUrl: './ficheros.component.html',
+  styleUrls: ['./ficheros.component.scss']
+})
+export class FicherosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
